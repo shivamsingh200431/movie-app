f9ca76e5521a117e94a21e6fd5e8ec33e70ae1ca
shivamsingh200431 <s.singhlmvm1@outlook.com>
2026-04-17 01:22:16 +0530

    frontend connected to backend

---
+++ script.js
@@ -0,0 +1,159 @@
+async function searchMovie() {
+  
+  const input = document.getElementById("searchInput").value;
+  const resultsDiv = document.getElementById("results");
+
+    if (!input) {
+  resultsDiv.innerHTML = "<p>Please enter a movie name</p>";
+  return;
+}
+if (input.trim() === "") {
+  resultsDiv.innerHTML = "<p>⚠️ Please enter a movie name</p>";
+  return;
+}
+
+  resultsDiv.innerHTML = "<p>Loading...</p>";
+
+  const apiKey = "b13ed654";
+  const url = `https://movie-backend-oyrj.onrender.com/search?q=${input}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    let html = "";
+
+    if (data.Search) {
+      data.Search.forEach(movie => {
+        // CARD HTML
+       html += `
+
+       
+  <div class="card">
+    <img 
+      src="${movie.Poster}" 
+      width="100"
+      onerror="this.onerror=null; this.src='fallback.jpg'"
+    >
+    <h3>${movie.Title}</h3>
+    <p>${movie.Year}</p>
+
+    <button onclick='addToFavorites(${JSON.stringify(movie)})'>
+      ❤️ Add
+    </button>
+  </div>
+`;
+      });
+    } else {
+      html = "<p>😕 No movies found. Try something else.</p>";
+    }
+
+    resultsDiv.innerHTML = html;
+
+  } catch (error) {
+    resultsDiv.innerHTML = "<p>Something went wrong</p>";
+    console.error(error);
+  }
+}
+async function getMovieDetails(id) {
+  const resultsDiv = document.getElementById("results");
+
+  resultsDiv.innerHTML = "<p>Loading details...</p>";
+
+  const apiKey = "b13ed654";
+  const url = `https://movie-backend-oyrj.onrender.com/movie/${id}`;
+
+  try {
+    const response = await fetch(url);
+    const movie = await response.json();
+
+    resultsDiv.innerHTML = `
+      <div class="card" style="width: 300px">
+        <img 
+          src="${movie.Poster}" 
+          width="200"
+          onerror="this.src='https://via.placeholder.com/200'"
+        >
+        <h2>${movie.Title}</h2>
+        <p><strong>Year:</strong> ${movie.Year}</p>
+        <p><strong>Genre:</strong> ${movie.Genre}</p>
+        <p><strong>IMDB Rating:</strong> ${movie.imdbRating}</p>
+        <p><strong>Plot:</strong> ${movie.Plot}</p>
+
+        <button onclick="goBack()">⬅ Back</button>
+      </div>
+    `;
+  } catch (error) {
+    resultsDiv.innerHTML = "<p>Error loading details</p>";
+  }
+}
+function goBack() {
+  document.getElementById("results").innerHTML = "<p>Search again</p>";
+}
+
+
+                            // Add to Favorites
+
+
+
+async function addToFavorites(movie) {
+  await fetch("https://movie-backend-oyrj.onrender.com/addFavorite", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(movie)
+  });
+
+  alert("Added to favorites!");
+}
+
+
+                            // Show Favorites
+
+async function showFavorites() {
+  const resultsDiv = document.getElementById("results");
+
+  const response = await fetch("https://movie-backend-oyrj.onrender.com/favorites");
+  const favorites = await response.json();
+
+  let html = "";
+
+  if (favorites.length === 0) {
+    html = "<p>No favorites yet</p>";
+  } else {
+    favorites.forEach(movie => {
+      const poster = movie.Poster !== "N/A" ? movie.Poster : "fallback.jpg";
+
+      html += `
+        <div class="card">
+          <img src="${poster}" width="100">
+          <h3>${movie.Title}</h3>
+          <p>${movie.Year}</p>
+
+          <button onclick="removeFromFavorites('${movie.imdbID}')">
+            ❌ Remove
+          </button>
+        </div>
+      `;
+    });
+  }
+
+  resultsDiv.innerHTML = html;
+}
+
+                // Remove from Favorites
+
+async function removeFromFavorites(id) {
+  await fetch(`https://movie-backend-oyrj.onrender.com/removeFavorite/${id}`);
+
+  showFavorites();
+}
+                 // Update Favorites Count
+
+function updateFavCount() {
+  let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+
+  document.getElementById("favCount").innerText =
+    "Favorites: " + favorites.length;
+}                 
